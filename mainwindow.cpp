4830b74b171579a6a5ed127e4cf70ea95b8deeec
wsdassssss <68675769+wsdassssss@users.noreply.github.com>
2020-09-21 15:38:22 +0800

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,135 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+/**************守护程序**********************/
+/************mimo431************************/
+/************2020-0921-1524****************/
+//待改进：
+//最小化到任务栏，右键完成各种操作,后台运行
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    init();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+void MainWindow::init()
+{
+    //初始化，开启计时器
+    time_count.setInterval(1000);//间隔时间
+
+    ui->btn_start->setEnabled(false);
+
+    record_path="C:/Users/Administrator/Desktop/record.txt";
+}
+void MainWindow::listen()
+{
+    p.start("tasklist",QStringList()<<"-fi"<<"imagename eq "+file_name);
+    p.waitForFinished();
+    tasklist=QString::fromLocal8Bit(p.readAllStandardOutput());
+    status_now=(tasklist.contains(file_name));
+    qDebug()<<"listening..."<<status_before<<"   "<<status_now<<"  "<<ui->checkBox->checkState();
+
+//    //初始化阶段
+//    if(status_start)
+//    {
+//        if(tasklist.contains(file_name))
+//        {
+//            ui->statusBar->showMessage("Running");
+//        }
+//        else {
+//            ui->statusBar->showMessage("Stop");
+//        }
+//        status_start=false;
+//        write("守护程序启动晚于软件启动时间，开启时间未知");
+//        tasklist.clear();
+//        status_before=status_now;
+//        return;
+//    }
+    //自动状态较为特殊，前后皆为true,
+    if(Qt::Checked==ui->checkBox->checkState())
+        if(status_now==false)
+        {
+            on_btn_start_clicked();
+        }
+    if(status_now!=status_before)
+    {
+       if(status_now==true){
+        //  write("first");
+          ui->statusBar->showMessage("Running");
+          write("Start:");
+       }
+       else
+       {
+            ui->statusBar->showMessage("Stop");
+            write("Stop:");
+            if(Qt::Checked==ui->checkBox->checkState())
+                on_btn_start_clicked();
+       }
+    }
+    tasklist.clear();
+    status_before=status_now;
+
+}
+
+void MainWindow::write(QString arg)
+{
+    QDateTime curTime=QDateTime::currentDateTime();
+    QString time_start=curTime.toString("yyyy-MM-dd hh:mm:ss");
+    QFile file_record(record_path);
+    if(!file_record.open(QIODevice::Append |QIODevice::Text))
+    {
+        qDebug()<<"someting wrong";
+    }
+    QTextStream in(&file_record);
+    in.setAutoDetectUnicode(true);
+    in<<file_name+" "+arg+": "+time_start<<"\n";
+    file_record.close();
+}
+void MainWindow::on_btn_open_clicked()
+{
+    file_path=QFileDialog::getOpenFileName(this,"找文件","C:/Users/Administrator/Desktop","*.exe");
+    ui->lineEdit->setText(file_path);
+    file_path=ui->lineEdit->text();
+    file_name=file_path.split("/").last();
+    qDebug()<<file_name;
+    time_count.start();
+    connect(&time_count,SIGNAL(timeout()),this,SLOT(listen()));
+}
+
+void MainWindow::on_checkBox_stateChanged(int arg1)
+{
+    qDebug()<<arg1;
+    listen();
+}
+
+void MainWindow::on_btn_start_clicked()
+{
+    if(ui->statusBar->currentMessage()=="Running")
+    {
+        return;
+    }
+    qDebug()<<"click start";
+    qDebug()<<file_path;
+    p.startDetached(file_path);
+
+
+}
+
+void MainWindow::on_btn_stop_clicked()
+{
+    QString cmd="taskkill /im "+file_name+" /f";
+    p.execute(cmd);
+
+}
+
+void MainWindow::on_lineEdit_textChanged(const QString &arg1)
+{
+    if(arg1.trimmed()!="")
+        ui->btn_start->setEnabled(true);
+    file_path=arg1;
+}
---
+++ mainwindow.h
@@ -0,0 +1,57 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMainWindow>
+#include "QFileDialog"
+#include "QFile"
+#include "QDateTime"
+#include "QDebug"
+#include "QTime"
+#include "QTimer"
+#include "QProcess"
+#include "QMessageBox"
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    QString file_path;//程序所在路径
+    QString file_name;//要守护的程序名
+
+    QString record_path;
+    QTimer time_count;
+    QProcess p;
+    QString tasklist;//当前运行程序
+
+    bool status_before=false;
+    bool status_now;
+
+    bool status_start=true;
+public:
+    void init();
+    void write(QString arg);
+private slots:
+    void listen();
+    void on_btn_open_clicked();
+
+    void on_checkBox_stateChanged(int arg1);
+
+    void on_btn_start_clicked();
+
+    void on_btn_stop_clicked();
+
+    void on_lineEdit_textChanged(const QString &arg1);
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
